6c5457b0ba525d26cd4b2f84dad436131dba4ffa
phunghung <phunghung0901xx@gmail.com>
2022-11-12 19:45:03 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,53 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-
+import "./App.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+  useQuery,
+} from "@apollo/client";
+interface Iprops {
+  id: number;
+  name: string;
+  description: string;
+  photo: string;
+}
 function App() {
-  return (
-    <div className="App">
-      
+  const GET_LOCATIONS = gql`
+    query GetLocations {
+      locations {
+        id
+        name
+        description
+        photo
+      }
+    }
+  `;
+  const GET_DOGS = gql`
+    query GetDogs {
+      dogs {
+        id
+        breed
+      }
+    }
+  `;
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.locations.map(({ id, name, description, photo }: Iprops) => (
+    <div key={id}>
+      <h3>{name}</h3>
+      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+      <br />
+      <b>About this location:</b>
+      <p>{description}</p>
+      <br />
     </div>
-  );
+  ));
 }
 
 export default App;
